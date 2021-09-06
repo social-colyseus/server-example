c6e338be6a101df13fc8c6a87f1e1e3595c57582
Orkun <orkun@promega.com.tr>
2021-09-06 16:49:27 +0300

    Initialize :tada:

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import {createServer} from 'http';
+import {Server} from 'colyseus';
+import {WebSocketTransport} from '@colyseus/ws-transport';
+import {SocialColyseusApp} from '@social-colyseus/server';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const server = createServer(app);
+
+const gameServer = new Server({
+    transport: new WebSocketTransport({
+        server,
+    }),
+});
+
+const socialApp = new SocialColyseusApp(gameServer, {
+    database: {
+        host: process.env.DB_HOST,
+        name: process.env.DB_NAME,
+        pass: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+    },
+});
+app.use('/', express.static('src/static'));
+app.use(socialApp.httpHandler(['/']));
+
+gameServer.listen(5567, '0.0.0.0').finally();
